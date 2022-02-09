cbaf113a2cccf8cf4e0deb5d2d0625bae0f96b74
juann11 <juan.ospinaa@upb.edu.co>
2022-02-09 10:23:22 -0800

    avance prueba 11

--- problem1/event.c
+++ problem1/event.c
@@ -1,10 +1,12 @@
 #include "event.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 Event *CreateEvent(char *name)
 {
-    Event *event = NULL;
+    Event *event = malloc(sizeof(Event));
+    sscanf(name,"%s", event->eventName);
     return event;
 }
 
--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,8 +5,8 @@
 
 EventList *CreateEventList(void)
 {
-    EventList *eventList = NULL;
-
+    EventList *eventList = malloc(sizeof(EventList));
+    eventList->isEmpty=0;
     return eventList;
 }
 
@@ -19,22 +19,87 @@ void DestroyEventList(EventList *this)
 
 Event *SearchEvent(EventList *this, char *name)
 {
-
+    Event *a = this->head;
+    if (this->isEmpty==0)
+    {
+        return NULL;
+    }
+    else 
+    {
+        while(a !=NULL)
+        {
+            if (*(a->eventName +2)==*(name +2)&& *(a->eventName+3)==*(name+3))
+            {
+                return a;
+            }
+            a=a->next;
+        }
+    }
 
     return NULL;
 }
 
 void AddEvent(EventList *this, Event *event)
 {
-
+    if (this->isEmpty==0)
+    {
+        this->head= event;
+        this->last= event;
+        this->isEmpty=1;
+    }
+    else
+    {
+        this->last->next=event;
+        this->last=event;
+    }
 }
 
 void RemoveEvent(EventList *this, char *name)
 {
+    Event *a = this->head;
+    if (this->isEmpty==0)
+    {
+        return;
+    }
+    else
+    {
+        while (a !=NULL)
+        {
+            if (*(this->head->eventName+2)==*(name +2))
+            {
+                this->head= this->head->next;
+                break;
+            }
+            else if (*(a->next->eventName+2)==*(name +2))
+            {
+                a->next=a->next->next;
+                break;
+            }
+            a = a->next;
+        }
+        if (this->head==NULL)
+        {
+            this->isEmpty=0;
+        }
+        
+    }
 }
 
 void ListEvents(EventList *this)
 {
     // imprime esto si la lista está vacía
-    printf("empty\n");
+    Event *a = this->head;
+    if (this->isEmpty==0)
+    {
+        printf("empty\n");
+    }
+    else
+    {
+        while (a !=NULL)
+        {
+            printf("%s\n",a->eventName);
+            a = a->next;
+        }
+        
+    }
 }

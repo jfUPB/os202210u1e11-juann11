3478c600a606865d88be1d59114d9d828c746993
juann11 <juan.ospinaa@upb.edu.co>
2022-02-11 09:27:14 -0800

    por fin

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,101 +5,126 @@
 
 EventList *CreateEventList(void)
 {
-    EventList *eventList = malloc(sizeof(EventList));
-    eventList->isEmpty=0;
+    EventList *eventList = malloc(sizeof(eventList));
+
     return eventList;
 }
 
 void DestroyEventList(EventList *this)
 {
-
-
     free(this);
 }
 
 Event *SearchEvent(EventList *this, char *name)
 {
+
     Event *a = this->head;
-    if (this->isEmpty==0)
+    if (this->isEmpty == 0)
     {
         return NULL;
     }
-    else 
+    
+    else
     {
-        while(a !=NULL)
+       
+        while (a != NULL)
         {
-            if (*(a->eventName +2)==*(name +2)&& *(a->eventName+3)==*(name+3))
+            if (*(a->eventName + 2) == *(name + 2) && *(a->eventName + 3) == *(name+3))
             {
                 return a;
             }
-            a=a->next;
+
+            a = a->next;
         }
     }
+
+    DestroyEvent(a);
 
     return NULL;
 }
 
 void AddEvent(EventList *this, Event *event)
 {
-    if (this->isEmpty==0)
-    {
-        this->head= event;
-        this->last= event;
-        this->isEmpty=1;
-    }
-    else
-    {
+    Event *a = this->head;
+
+    if (this->isEmpty!=0){
+        while (a != NULL)
+        {
+            if(*(a->eventName+2)==*(event->eventName+2)&& *(a->eventName+3)==*(event->eventName+3)){
+                return;
+            }
+
+            a = a->next;
+        }
+
         this->last->next=event;
         this->last=event;
     }
+    else{
+        this->head = event;
+        this->last = event;
+        this->isEmpty= 1;
+    }
+    DestroyEvent(a);
 }
 
 void RemoveEvent(EventList *this, char *name)
 {
+    
     Event *a = this->head;
-    if (this->isEmpty==0)
+
+    Event *t = SearchEvent(this, name);
+
+    if (t == NULL)
+        return;
+    if (this->isEmpty == 0)
     {
         return;
     }
+    
     else
     {
-        while (a !=NULL)
+       
+        while (a != NULL)
         {
-            if (*(this->head->eventName+2)==*(name +2))
+            
+            
+            if (*(this->head->eventName + 2) == *(name + 2))
             {
-                this->head= this->head->next;
+                this->head = this->head->next;
                 break;
             }
-            else if (*(a->next->eventName+2)==*(name +2))
+
+            else if (*(a->next->eventName + 2) == *(name + 2))
             {
-                a->next=a->next->next;
+                a->next = a->next->next;
                 break;
             }
+
             a = a->next;
+            
         }
         if (this->head==NULL)
         {
-            this->isEmpty=0;
+            this->isEmpty = 0;
         }
-        
     }
 }
 
 void ListEvents(EventList *this)
 {
-    // imprime esto si la lista está vacía
     Event *a = this->head;
-    if (this->isEmpty==0)
-    {
+    
+    if (this->isEmpty == 0)
         printf("empty\n");
-    }
+        
     else
     {
-        while (a !=NULL)
+        while (a != NULL)
         {
-            printf("%s\n",a->eventName);
+            printf("%s\n", a->eventName); // BORRAR OJO
             a = a->next;
         }
-        
     }
+    DestroyEvent(a);
 }

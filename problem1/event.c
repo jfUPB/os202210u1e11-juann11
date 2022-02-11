59fb3d3781953a0210aae338b9c570ffce5e13b4
juann11 <juan.ospinaa@upb.edu.co>
2022-02-11 09:42:23 -0800

    trabajo

--- problem1/event.c
+++ problem1/event.c
@@ -6,7 +6,9 @@
 Event *CreateEvent(char *name)
 {
     Event *event = malloc(sizeof(Event));
+    name[15]=0;
     sscanf(name,"%s", event->eventName);
+    event->next = NULL;
     return event;
 }
 
